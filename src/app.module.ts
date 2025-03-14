3def6a048db9c3601c14ad0ff13969f1102537d0
rasyadsr <mchmdrasyad@gmail.com>
2025-03-14 10:06:28 +0700

    feat: apply auth & common module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import dataSource from './config/database.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SilabusModule } from './modules/silabus/silabus.module';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSource.options),
     SilabusModule,
     UserModule,
+    AuthModule,
+    CommonModule,
   ],
 })
 export class AppModule {}
