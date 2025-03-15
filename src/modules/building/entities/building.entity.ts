4f559aebbea4169d5b5a1f2116cfff7e2f7ac182
sandifauzan01 <sandi.fauzan@hiu.co>
2025-03-15 14:39:32 +0700

    feat: enhance Building and Room entities with properties and relationships

--- src/modules/building/entities/building.entity.ts
+++ src/modules/building/entities/building.entity.ts
@@ -1,1 +1,18 @@
-export class Building {}
+import { AbstractEntity } from 'src/common/database/abstract.entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity({
+    name: 'building',
+})
+
+export class Building extends AbstractEntity<Building> {
+    @Column({ type: 'varchar', length: 100, nullable: false })
+    name: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    floor: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    status: string;
+
+}
--- src/modules/room/entities/room.entity.ts
+++ src/modules/room/entities/room.entity.ts
@@ -1,6 +1,6 @@
 import { AbstractEntity } from 'src/common/database/abstract.entity';
 import { Column, Entity, ManyToOne, JoinColumn } from 'typeorm';
-// import { Building } from 'src/modules/building/entities/building.entity';
+import { Building } from 'src/modules/building/entities/building.entity';
 
 @Entity({
     name: 'room',
@@ -16,9 +16,9 @@ export class Room extends AbstractEntity<Room> {
     @Column({ type: 'integer' })
     building_id: number;
 
-    // @ManyToOne(() => Building)
-    // @JoinColumn({ name: 'building_id' })
-    // building: Building;
+    @ManyToOne(() => Building)
+    @JoinColumn({ name: 'building_id' })
+    building: Building;
 
     @Column({ type: 'varchar', length: 255 })
     status: string;
