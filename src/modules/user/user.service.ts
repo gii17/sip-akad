947db1ee3fd8f78065731118aaf3e1f22a3c04f4
mgifariamtlh <mgifari2005@gmail.com>
2025-03-24 10:52:20 +0700

    Hash password when updating user entity

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,6 +2,7 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { FindAllUserDto } from './dto/find-all-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -17,6 +18,10 @@ export class UserService {
 
   async update(id: string, updateUserDto: UpdateUserDto) {
     try {
+      // Hash password user ketika update entity USER
+      if (updateUserDto.password) {
+        updateUserDto.password = await hash(updateUserDto.password, 10);
+      }
       const user = await this.userRepository.findOneAndUpdate(
         {
           id,
